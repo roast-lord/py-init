2743ff5a17ba68ff7ed9b56ac4b7effa0fa75ba9
Roast-Lord <siloegarcez@gmail.com>
2023-07-13 09:03:54 -0300

    First Commit

---
+++ py-init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+current_dir=$(pwd)
+dir_name=$(basename "$current_dir")
+
+read -p "Which python version?: " version
+
+asdf plugin update python
+
+asdf install python $version
+
+asdf local python $version
+
+echo "Python $version has been installed successfully."
+
+poetry init --name $dir_name --python $version --dev-dependency pylint@latest --dev-dependency mypy@latest --dev-dependency black@latest --dev-dependency isort@latest --dev-dependency flake8@latest
+
+git init -b main
+touch .env
+touch .gitignore
+touch .tool-versions
+touch README.md
+echo ".env" >>.gitignore
+
+mkdir src
+mkdir tests
+mkdir src/$dir_name
+touch src/$dir_name/__init__.py
+touch tests/__init__.py
